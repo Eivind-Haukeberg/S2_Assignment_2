f1c6403d32113abeb1a4a9ce7b21d46048a32599
Eivind Haukeberg <e.haukeberg@gmail.com>
2025-05-12 19:21:37 +0200

    create one JS for all dom selectors

---
+++ src/js/domSelector.js
@@ -0,0 +1,12 @@
+export const selectors = {
+  form: document.querySelector('.pharmacy-inventory__form'),
+  nameInput: document.querySelector('.pharmacy-inventory__input--product-name'),
+  manufacturerInput: document.querySelector(
+    '.pharmacy-inventory__input--manufacturer'
+  ),
+  expirationInput: document.querySelector(
+    '.pharmacy-inventory__input--expiration-date'
+  ),
+  quantityInput: document.querySelector('.pharmacy-inventory__input--quantity'),
+  tableBodySelector: '.pharmacy-inventory__table-body',
+};
