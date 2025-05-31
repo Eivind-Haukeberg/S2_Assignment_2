efb7c164ffb8f2892165045a4a293e894b038365
Eivind Haukeberg <e.haukeberg@gmail.com>
2025-05-31 17:26:07 +0200

    update medice.js for inheritance

--- src/js/Medicine.js
+++ src/js/Medicine.js
@@ -1,11 +1,10 @@
 import { v4 as uuidv4 } from 'uuid';
 import { Product } from './Product.js';
 
-export class Medicine {
+export class Medicine extends Product {
   constructor(name, manufacturer, expirationDate, quantity) {
+    super(name, manufacturer);
     this.id = uuidv4();
-    this.name = name;
-    this.manufacturer = manufacturer;
     this.expirationDate = new Date(expirationDate).toISOString();
     this.quantity = quantity;
   }
